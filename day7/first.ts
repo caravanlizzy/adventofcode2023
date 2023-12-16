77a3c1cd508f37fe831185f87c101a4716a4da10
caravanlizzy <marc.lambrecht@gmx.de>
2023-12-16 01:47:20 +0100

    solve day7

--- day7/first.ts
+++ day7/first.ts
@@ -17,65 +17,68 @@ class Puzzle{
     return rowString.split(' ');
   }
 
-  sortString(hand:string):string{
-    return hand.split('').sort().join('');
+  sortString(str:string):string{
+    return str.split('').sort().join('');
   }
 
-  sortStringReverse(hand:string):string{
-    return hand.split('').sort().reverse().join('');
+  sortStringReverse(str:string):string{
+    return str.split('').sort().reverse().join('');
   }
 
-  getChanges(hand:string):string{
-    let changes = '';
+  getSequence(cards:string):string{
+    let sequence = '';
     let counter = 1;
-    for(let i = 1; i < hand.length; i++){
-      if(hand[i] === hand[i-1]){
+    const sortedCards = this.sortString(cards);
+    for(let i = 1; i < cards.length; i++){
+      if(sortedCards[i] === sortedCards[i-1]){
         counter += 1;
       } else{
-        counter + 1;
-        changes += counter;
+        sequence += counter;
+        counter = 1;
       }
     }
-    const reversed = this.sortStringReverse(hand);
-    console.log(reversed);
-    return reversed;
+    sequence += counter;
+    return this.sortStringReverse(sequence);
   }
 
-  getHandValue(hand:string){
+  getHandValue(cards:string){
     let value = 0;
-    let handType = this.getHandType(hand);
+    let handType = this.getHandType(cards);
     value += handType * 16**6;
     let power = 5;
-    for(let i = 0; i < hand.length; i++){
-      value += this.cardToNumber(hand) * 16**power;
+    for(let i = 0; i < cards.length; i++){
+      value += this.cardToNumber(cards[i]) * 16**power;
+      power -= 1;
     }
     return value;
   }
 
   getHandType(hand: string):number{
-    let changes = this.getChanges(hand);
-    if(changes == '5') return 13;
-    if(changes == '41') return 12;
-    if(changes == '32') return 11;
-    if(changes == '311') return 10;
-    if(changes == '221') return 9;
-    if(changes == '2111') return 8;
-    if(changes == '11111') return 7;
+    let sequence = this.getSequence(hand);
+    if(sequence == '5') return 13;
+    if(sequence == '41') return 12;
+    if(sequence == '32') return 11;
+    if(sequence == '311') return 10;
+    if(sequence == '221') return 9;
+    if(sequence == '2111') return 8;
+    if(sequence == '11111') return 7;
     else{
-      console.log('Not in valid change range: '+ changes);
+      console.log('Not in valid change range: '+ sequence);
       return 0;
     }
   }
 
   createHandObject(hand:string):Hand{
       let [cards, bid] = hand.split(' ');
-      return { cards: cards, bid: parseInt(bid), value: this.getHandValue(hand) }
+      return { cards: cards, bid: parseInt(bid), value: this.getHandValue(cards) }
   } 
 
   createHandList(data:string[]):Hand[]{
     let hands:Hand[] = [];
     for(let hand of data){
-      hands.push(this.createHandObject(hand));
+      if(hand){
+        hands.push(this.createHandObject(hand));
+      }
     }
     return hands;
   }
@@ -97,12 +100,21 @@ class Puzzle{
     return 0;
   }
 
+  sortHands(hands:Hand[]):Hand[]{
+    return hands.sort((a,b) => a.value-b.value);
+  }
+
+  getProduct(hands:Hand[]):number{
+    return hands.reduce((total, current, index) => total+current.bid*(index + 1), 0);
+  }
+
   async run(){
     const dataString = await this.getData();
     const data = this.toArrray(dataString);
     const hands = this.createHandList(data);
-    console.log(hands);
-    //    console.log( this.transformData(data) );
+    const sortedHands = this.sortHands(hands);
+    const product = this.getProduct(sortedHands);
+    console.log(product);
   }
 }
 
